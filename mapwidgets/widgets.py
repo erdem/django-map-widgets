fb42e8a6d6c09352a4f6070654d610f7f90f8e17
erdem <erdemozkol@gmail.com>
2016-02-29 19:13:22 +0200

    implemented django widget class for google map widget

--- mapwidgets/widgets.py
+++ mapwidgets/widgets.py
@@ -1,1 +1,32 @@
+from django import forms
+from django.conf import settings
+from django.template.loader import render_to_string
 
+
+class GoogleMapWidget(forms.Textarea):
+    template = "mapwidgets/google-map-widget.html"
+
+    class Media:
+        css = {
+            "all": ("mapwidgets/css/bootstrap.css",
+                    "mapwidgets/css/bootstrap-theme.css")
+        }
+
+        js = ('https://maps.googleapis.com/maps/api/js?libraries=places',
+              "mapwidgets/js/jquery_class.min.js",
+              "mapwidgets/js/django_mw_base.js",
+              "mapwidgets/js/django_mw_google_map.js",
+        )
+
+    def render(self, name, value, attrs=None):
+        textarea = super(GoogleMapWidget, self).render(name, value, attrs)
+        data = {
+            "textarea": textarea,
+            "name": name,
+            "value": value,
+            "widget": self,
+            "options": self.get_map_options(),
+            "STATIC_URL": settings.STATIC_URL,
+            "textarea_attrs": attrs
+        }
+        return render_to_string(self.template, data)
