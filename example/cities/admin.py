eee3242b393eed5b5fb46c5ddb40a981223d5143
erdem <erdemozkol@gmail.com>
2016-03-01 02:28:07 +0200

    implemented google map widget example project admin

--- example/cities/admin.py
+++ example/cities/admin.py
@@ -1,5 +1,8 @@
+from django import forms
 from django.contrib.gis import admin
 from cities.models import City, District
+from django.contrib.gis.db import models
+from mapwidgets.widgets import GoogleMapWidget
 
 
 class DistrictAdminInline(admin.TabularInline):
@@ -7,8 +10,17 @@ class DistrictAdminInline(admin.TabularInline):
     extra = 3
 
 
+class CityAdminForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = "__all__"
+        widgets = {
+            'coordinates': GoogleMapWidget,
+        }
+
 class CityAdmin(admin.ModelAdmin):
     inlines = (DistrictAdminInline,)
+    form = CityAdminForm
 
 
 class DistrictAdmin(admin.ModelAdmin):
--- example/example/settings.py
+++ example/example/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
 
+    'mapwidgets',
+
     'cities'
 ]
 
