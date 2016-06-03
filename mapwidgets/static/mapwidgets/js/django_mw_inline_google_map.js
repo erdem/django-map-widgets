f42ca8357d318a42fa78ac82e0d5b985ab58da4c
erdem <erdemozkol@gmail.com>
2016-06-03 18:11:01 +0100

    DjangoInlineGoogleMapWidget js class improvements

--- mapwidgets/static/mapwidgets/js/django_mw_inline_google_map.js
+++ mapwidgets/static/mapwidgets/js/django_mw_inline_google_map.js
@@ -5,6 +5,15 @@ DjangoInlineGoogleMapWidget = DjangoGoogleMapWidget.extend({
 
     init: function (options) {
         $.extend(this, options);
-        this.Super()
+        this.Super();
+        setTimeout(function () {
+            this.addInlineRowBtn = $(this.eee, "body");
+            console.log(this.addInlineRowBtn)
+            this.addInlineRowBtn.on("click", this.handleaddInlineRowBtnClick.bind(this));
+        }.bind(this), 500);
+    },
+
+    handleaddInlineRowBtnClick: function (e) {
+        alert("asd");
     }
 });
