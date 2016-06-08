d0ba17af3b28d0c8868493029c032f4b938668d1
erdem <erdemozkol@gmail.com>
2016-06-08 14:26:45 +0100

    DjangoGoogleMapWidget temporary parameter fixes

--- mapwidgets/widgets.py
+++ mapwidgets/widgets.py
@@ -55,15 +55,24 @@ class GoogleMapWidget(BaseGeometryWidget):
 class InlineMapWidgetMixin(object):
     inline_add_row_jquery_selector = None
 
-    def map_options(self):
+    def get_js_widget_data(self, name, element_id):
         if not self.inline_add_row_jquery_selector:
             raise ImproperlyConfigured(
                 "The '%s' does not specify "
                 "a 'inline_add_row_jquery_selector'." % self.__class__.__name__)
 
-        map_conf = mw_settings.map_conf
-        map_conf["inline_add_row_jquery_selector"] = self.inline_add_row_jquery_selector
-        return json.dumps(mw_settings.map_conf)
+        map_elem_selector = "#%s-mw-wrap" % name
+        map_elem_id = "%s-map-elem" % name
+        google_auto_input_id = "%s-mw-google-address-input" % name
+        location_input_id = "#%s" % element_id
+        js_widget_params = {
+            "inline_add_row_jquery_selector": self.inline_add_row_jquery_selector,
+            "wrapElemSelector": map_elem_selector,
+            "mapElemID": map_elem_id,
+            "googleAutoInputID": google_auto_input_id,
+            "locationInputID": location_input_id
+        }
+        return js_widget_params
 
 
 class GoogleInlineMapWidget(InlineMapWidgetMixin, GoogleMapWidget):
@@ -86,27 +95,16 @@ class GoogleInlineMapWidget(InlineMapWidgetMixin, GoogleMapWidget):
             "mapwidgets/js/django_mw_google_map.js",
         )
 
-    def get_js_widget_data(self, name, element_id):
-        map_elem_selector = "#%s-mw-wrap" % name
-        map_elem_id = "%s-map-elem" % name
-        google_auto_input_id = "%s-mw-google-address-input" % name
-        location_input_id = "#%s" % element_id
-
-        js_widget_params = {
-            "wrapElemSelector": map_elem_selector,
-            "mapElemID": map_elem_id,
-            "googleAutoInputID": google_auto_input_id,
-            "locationInputID": location_input_id
-        }
-        return json.dumps(js_widget_params)
-
     def render(self, name, value, attrs=None):
         if not attrs:
             attrs = dict()
 
         element_id = attrs.get("id")
+        is_formset_empty_from_template = "__prefix__" in element_id
+        widget_data = self.get_js_widget_data(name, element_id)
         attrs.update({
-            "js_widget_data": self.get_js_widget_data(name, element_id)
+            "js_widget_data": json.dumps(widget_data),
+            "is_formset_empty_from_template": is_formset_empty_from_template
         })
         return super(GoogleInlineMapWidget, self).render(name, value, attrs)
 
