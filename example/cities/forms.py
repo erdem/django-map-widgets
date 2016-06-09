75355a4cb1a4afca4e13e352935020c4c4152ac5
erdem <erdemozkol@gmail.com>
2016-06-09 23:37:04 +0100

    various example project improvements

--- example/cities/forms.py
+++ example/cities/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from cities.models import City
+from mapwidgets.widgets import GoogleMapWidget
 
 
 class CityForm(forms.ModelForm):
@@ -8,3 +9,7 @@ class CityForm(forms.ModelForm):
     class Meta:
         model = City
         fields = ("name", "coordinates", "city_hall")
+        widgets = {
+            'coordinates': GoogleMapWidget,
+            'city_hall': GoogleMapWidget,
+        }
