7808f0799a1eec881f98f371655e14961c85f361
erdem <erdemozkol@gmail.com>
2016-06-09 23:46:24 +0100

    various example project improvements

--- example/cities/urls.py
+++ example/cities/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from cities.views import CityCreateView
+from cities.views import CityCreateView, CityListView
 
 urlpatterns = [
-    # url(r'^$', CityListView.as_view(), name="list"),
+    url(r'^$', CityListView.as_view(), name="list"),
     url(r'^create/$', CityCreateView.as_view(), name="create"),
 ]
--- example/cities/views.py
+++ example/cities/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import FormView, ListView
 
 from cities.forms import CityForm
+from cities.models import City
 
 
 class CityListView(ListView):
+    queryset = City.objects.all()
     template_name = "cities/list.html"
     context_object_name = "cities"
 
