e0877c140444215447656cc96cb625f560909d71
erdem <erdemozkol@gmail.com>
2016-06-09 23:45:26 +0100

    various example project improvements

--- example/cities/views.py
+++ example/cities/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 
 from cities.forms import CityForm
+
+
+class CityListView(ListView):
+    template_name = "cities/list.html"
+    context_object_name = "cities"
 
 
 class CityCreateView(FormView):
     template_name = "cities/form.html"
     form_class = CityForm
+
 
--- example/example/urls.py
+++ example/example/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('cities:list'))),
     url(r'^admin/', admin.site.urls),
     url(r'^cities/', include('cities.urls', namespace='cities')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
