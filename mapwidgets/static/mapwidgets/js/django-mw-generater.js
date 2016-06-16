3b63aa4e8db7560750a5aa4b7da92c3ab6e15718
erdem <erdemozkol@gmail.com>
2016-06-16 18:34:51 +0100

    jquery class fixes

--- mapwidgets/static/mapwidgets/js/django-mw-generater.js
+++ mapwidgets/static/mapwidgets/js/django-mw-generater.js
@@ -3,7 +3,7 @@
 
         init: function (options) {
             $.extend(this, options);
-            $(document).on("click", this.inline_add_row_jquery_selector, this.handleInlineAddRowBtn.bind(this));
+            $(document).on('formset:added', this.handle_added_formset_row.bind(this));
 
         },
 
@@ -11,37 +11,12 @@
 
         },
 
-        handleInlineAddRowBtn: function (e) {
+        handle_added_formset_row: function (e) {
 
             var mapOptions = this.mapOptions;
             var widgetData = this.getWidgetData();
 
-            // var wrapElemSelector = widgetData.wrapElemSelector;
-            // var mapElemID = widgetData.mapElemID;
-            // var googleAutoInputID = widgetData.googleAutoInputID;
-            // var locationInputID = widgetData.locationInputID;
-            //
-            // var mapWidgetOptions = {
-            //     locationInput: $(locationInputID),
-            //     wrapElemSelector: wrapElemSelector,
-            //     locationFieldValue: JSON.parse("{{ field_value|escapejs }}"),
-            //     mapApiKey: null,
-            //     mapElement: document.getElementById(mapElemID),
-            //     mapCenterLocationName: mapOptions.mapCenterLocationName,
-            //     mapCenterLocation: mapOptions.mapCenterLocation,
-            //     coordinatesOverlayToggleBtn: $(".mw-btn-coordinates", wrapElemSelector),
-            //     coordinatesOverlayDoneBtn: $(".mw-btn-coordinates-done", wrapElemSelector),
-            //     coordinatesOverlayInputs: $(".mw-overlay-input", wrapElemSelector),
-            //     coordinatesOverlay: $(".mw-coordinates-overlay", wrapElemSelector),
-            //     myLocationBtn: $(".mw-btn-my-location", wrapElemSelector),
-            //     addressAutoCompleteInput: document.getElementById(googleAutoInputID),
-            //     deleteBtn: $(".mw-btn-delete", wrapElemSelector),
-            //     addMarkerBtn: $(".mw-btn-add-marker", wrapElemSelector),
-            //     loaderOverlayElem: $(".mw-loader-overlay", wrapElemSelector),
-            //     zoom: mapOptions.zoom
-            // };
-            // new DjangoGoogleMapWidget(mapWidgetOptions);
         }
     });
-})(django.jQuery);
+})(jQuery || django.jQuery);
 
