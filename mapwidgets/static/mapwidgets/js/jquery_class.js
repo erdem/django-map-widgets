ddb6c6f063ae2074cf1f94f3486393e4f1c8a6e8
erdem <erdemozkol@gmail.com>
2016-06-17 16:08:23 +0100

    jquery class updates

---
+++ mapwidgets/static/mapwidgets/js/jquery_class.js
@@ -0,0 +1,72 @@
+(function($) {
+
+    var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\bSuper\b/ : /.*/;
+
+    $.Class = function() {
+
+    };
+
+    $.Class.extend = function(prop) {
+        var Super = this.prototype;
+
+        initializing = true;
+        var prototype = new this();
+        initializing = false;
+
+        for (var name in prop) {
+            prototype[name] = typeof prop[name] == "function" && typeof Super[name] == "function" && fnTest.test(prop[name]) ? (function(name, fn) {
+                return function() {
+                    var tmp = this.Super;
+
+                    this.Super = Super[name];
+
+                    var ret = fn.apply(this, arguments);
+                    this.Super = tmp;
+
+                    return ret;
+                };
+            })(name, prop[name]) : prop[name];
+        }
+
+        function Class() {
+            if (!initializing && this.init) {
+                this.init.apply(this, arguments);
+            }
+        }
+
+        Class.prototype = prototype;
+
+        Class.constructor = Class;
+
+        Class.extend = arguments.callee;
+
+        return Class;
+    };
+
+    if (typeof Function.bind === 'undefined') {
+
+        Function.prototype.bind = function(obj) {
+            var method = this;
+
+            tmp = function() {
+                return method.apply(obj, arguments);
+            };
+
+            return tmp;
+        };
+
+    }
+
+    if (!Array.indexOf){
+        Array.prototype.indexOf = function(obj) {
+            for (var i = 0; i < this.length; i++) {
+                if (this[i] == obj) {
+                    return i;
+                }
+            }
+
+            return -1;
+        };
+    }
+
+})(jQuery || django.jQuery);
