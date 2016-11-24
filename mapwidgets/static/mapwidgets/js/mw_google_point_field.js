a4d70e7ff5c2b4049ef457ff9454fb56bf1638f7
jose ariza <jariza@swapps.co>
2016-11-24 17:28:25 -0500

    commented fitBoundMarker to bypass zoom error

--- mapwidgets/static/mapwidgets/js/mw_google_point_field.js
+++ mapwidgets/static/mapwidgets/js/mw_google_point_field.js
@@ -59,17 +59,17 @@
             this.marker.addListener("dragend", this.dragMarker.bind(this));
         },
 
-        fitBoundMarker: function () {
-            var bounds = new google.maps.LatLngBounds();
-            bounds.extend(this.marker.getPosition());
-            this.map.fitBounds(bounds);
-            var listener = google.maps.event.addListener(this.map, "bounds_changed", function() {
-                if (this.getZoom() > 15) {
-                    this.setZoom(15)
-                }
-                google.maps.event.removeListener(listener);
-            });
-        },
+        // fitBoundMarker: function () {
+        //     var bounds = new google.maps.LatLngBounds();
+        //     bounds.extend(this.marker.getPosition());
+        //     this.map.fitBounds(bounds);
+        //     var listener = google.maps.event.addListener(this.map, "bounds_changed", function() {
+        //         if (this.getZoom() > 15) {
+        //             this.setZoom(15)
+        //         }
+        //         google.maps.event.removeListener(listener);
+        //     });
+        // },
 
         removeMarker: function(e){
             if (this.marker){
