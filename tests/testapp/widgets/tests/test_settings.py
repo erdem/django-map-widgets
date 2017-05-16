fa38b7b269b44571315743e96cfaf6b15cc5e92f
erdem <erdemozkol@gmail.com>
2017-05-16 15:26:29 +0100

    mapwidgets settings module test coverage %100

--- tests/testapp/widgets/tests/test_settings.py
+++ tests/testapp/widgets/tests/test_settings.py
@@ -1,14 +1,13 @@
-import json
 from collections import OrderedDict
 
 from django.test import TestCase
 from django.test.utils import override_settings
+from django.utils.six.moves import reload_module
 
 from mapwidgets.settings import MapWidgetSettings, DEFAULTS
 
 
 class WidgetSettingsTests(TestCase):
-
     def test_default_settings_values(self):
         mw_settings = MapWidgetSettings()
 
@@ -51,5 +50,21 @@ class WidgetSettingsTests(TestCase):
             self.assertEqual(google_point_static_widget_settings.get("zoom"), zoom)
             self.assertEqual(google_point_static_widget_settings.get("size"), static_map_size)
 
-        # test settings value validation
+    def test_settings_validations(self):
+        with override_settings(MAP_WIDGETS="invalid_map_widgets_settings_type"):
+            self.assertRaises(TypeError, lambda: getattr(MapWidgetSettings(), "GooglePointFieldWidget"))
+
+        invalid_tuple_settings = {
+            "GooglePointFieldWidget": (
+                ("zoom", 12, "invalid_value"),
+            ),
+        }
+
+        with override_settings(MAP_WIDGETS=invalid_tuple_settings):
+            self.assertRaises(ValueError, lambda: getattr(MapWidgetSettings(), "GooglePointFieldWidget"))
+
+        # test custom settings parameter validations
         self.assertRaises(TypeError, lambda: MapWidgetSettings(app_settings=1))
+
+        # test defaults parameter with invalid value
+        self.assertRaises(ValueError, lambda: getattr(MapWidgetSettings(defaults=invalid_tuple_settings), "GooglePointFieldWidget"))
