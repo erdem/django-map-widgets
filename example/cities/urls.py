373b6a8bce16ea53f0bd2d7f6242f70110faff57
erdem <github@erdemozkol.com>
2020-03-12 18:21:56 +0000

    updated django dockerizing flow

--- example/cities/urls.py
+++ example/cities/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from cities.views import CityCreateView, CityListView, CityDetailView
 
+app_name = 'cities'
+
 urlpatterns = [
-    url(r'^$', CityListView.as_view(), name="list"),
-    url(r'^(?P<pk>\d+)/$', CityDetailView.as_view(), name="detail"),
-    url(r'^create/$', CityCreateView.as_view(), name="create"),
+    path(r'^$', CityListView.as_view(), name="list"),
+    path(r'^(?P<pk>\d+)/$', CityDetailView.as_view(), name="detail"),
+    path(r'^create/$', CityCreateView.as_view(), name="create"),
 ]
