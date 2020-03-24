668ef61b6eae13a0b14b89ab1bf381a841a00404
erdem <github@erdemozkol.com>
2020-03-24 00:51:15 +0000

    fixed demo project namings

--- demo/example/wsgi.py
+++ demo/example/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for example project.
+WSGI config for demo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
 
 application = get_wsgi_application()
--- demo/manage.py
+++ demo/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'example.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
