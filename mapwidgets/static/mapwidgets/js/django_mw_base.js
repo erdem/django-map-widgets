2adedb9e52ac21ce314c8a64c5f155f46a8b50c0
Fergus Leahy <fergus@withtab.com>
2020-08-14 16:46:43 +0100

    bugfix: fix undefined place.geometry bug in handleAutoCompletePlaceChange

--- mapwidgets/static/mapwidgets/js/django_mw_base.js
+++ mapwidgets/static/mapwidgets/js/django_mw_base.js
@@ -1,6 +1,6 @@
 (function($) {
 	DjangoMapWidgetBase = $.Class.extend({
-		
+
 		init: function(options){
 			$.extend(this, options);
 			this.coordinatesOverlayToggleBtn.on("click", this.toggleCoordinatesOverlay.bind(this));
@@ -9,57 +9,57 @@
 			this.addMarkerBtn.on("click", this.handleAddMarkerBtnClick.bind(this));
 			this.myLocationBtn.on("click", this.handleMyLocationBtnClick.bind(this));
 			this.deleteBtn.on("click", this.resetMap.bind(this));
-			
+
 			// if the the location field in a collapse on Django admin form, the map need to initialize again when the collapse open by user.
 			if ($(this.wrapElemSelector).closest('.module.collapse').length){
 				$(document).on('show.fieldset', this.initializeMap.bind(this));
 			}
-			
+
 			var autocomplete = new google.maps.places.Autocomplete(this.addressAutoCompleteInput, this.GooglePlaceAutocompleteOptions);
 			google.maps.event.addListener(autocomplete, 'place_changed', this.handleAutoCompletePlaceChange.bind(this, autocomplete));
 			google.maps.event.addDomListener(this.addressAutoCompleteInput, 'keydown', this.handleAutoCompleteInputKeyDown.bind(this));
 			this.geocoder = new google.maps.Geocoder;
 			this.initializeMap.bind(this)();
 		},
-		
+
 		initializeMap: function(){
 			console.warn("Implement initializeMap method.");
 		},
-		
+
 		updateMap: function(lat, lng){
 			console.warn("Implement updateMap method.");
 		},
-		
+
 		addMarkerToMap: function(lat, lng){
 			console.warn("Implement this method for your map js library.");
 		},
-		
+
 		fitBoundMarker: function(){
 			console.warn("Implement this method for your map js library.");
 		},
-		
+
 		removeMarker: function(){
 			console.warn("Implement this method for your map js library.");
 		},
-		
+
 		dragMarker: function(e){
 			console.warn("Implement dragMarker method.");
 		},
-		
+
 		handleMapClick: function(e){
 			console.warn("Implement handleMapClick method.");
 		},
-		
+
 		handleAddMarkerBtnClick: function(e){
 			console.warn("Implement handleAddMarkerBtnClick method.");
 		},
-		
+
 		isInt : function(value) {
 			return !isNaN(value) &&
 				parseInt(Number(value)) === value &&
 				!isNaN(parseInt(value, 10));
 		},
-		
+
 		getLocationValues: function(){
 			var latlng = this.locationInput.val().split(' ');
 			var lat = latlng[2].replace(/[\(\)]/g, '');
@@ -69,7 +69,7 @@
 				"lng": lng
 			}
 		},
-		
+
 		callPlaceTriggerHandler: function (lat, lng, place) {
 			if (place === undefined){
                 var latlng = {lat: parseFloat(lat), lng: parseFloat(lng)};
@@ -88,7 +88,7 @@
 				);
 			}
 		},
-		
+
 		updateLocationInput: function(lat, lng, place){
 			var location_input_val = "POINT (" + lng + " " + lat + ")";
 			this.locationInput.val(location_input_val);
@@ -103,7 +103,7 @@
 					[place, lat, lng, this.wrapElemSelector, this.locationInput]
 				);
 			}
-			
+
 			this.callPlaceTriggerHandler(lat, lng, place);
 			this.locationFieldValue = {
 				"lng": lng,
@@ -111,7 +111,7 @@
 			};
 			this.deleteBtn.removeClass("mw-btn-default disabled").addClass("mw-btn-danger");
 		},
-		
+
 		resetMap: function(){
 			if (!$.isEmptyObject(this.locationFieldValue)) {
 				this.hideOverlay();
@@ -132,17 +132,17 @@
 				this.locationFieldValue = null;
 			}
 		},
-		
+
 		toggleCoordinatesOverlay: function(){
 			this.coordinatesOverlayToggleBtn.toggleClass("active");
 			$(".mw-coordinates-overlay", this.wrapElemSelector).toggleClass("hide");
 		},
-		
+
 		updateCoordinatesInputs: function(lat, lng){
 			$(".mw-overlay-latitude", this.wrapElemSelector).val(lat || "");
 			$(".mw-overlay-longitude", this.wrapElemSelector).val(lng || "");
 		},
-		
+
 		handleCoordinatesInputsChange: function (e) {
 			var lat = $(".mw-overlay-latitude", this.wrapElemSelector).val();
 			var lng = $(".mw-overlay-longitude", this.wrapElemSelector).val();
@@ -151,12 +151,12 @@
 				this.fitBoundMarker();
 			}
 		},
-		
+
 		handleCoordinatesOverlayDoneBtnClick: function(){
 			$(".mw-coordinates-overlay", this.wrapElemSelector).addClass("hide");
 			this.coordinatesOverlayToggleBtn.removeClass("active");
 		},
-		
+
 		handleMyLocationBtnClick: function(){
 			this.showOverlay();
 			if(navigator.geolocation){
@@ -167,20 +167,20 @@
 			}else{
 				this.handlecurrentPositionError();
 			}
-			
+
 		},
-		
+
 		handleCurrentPosition: function(location){
 			this.updateLocationInput(location.coords.latitude, location.coords.longitude);
 			this.hideOverlay();
 			this.fitBoundMarker();
 		},
-		
+
 		handlecurrentPositionError: function(){
 			this.hideOverlay();
 			alert("Your location could not be found.");
 		},
-		
+
 		handleAutoCompleteInputKeyDown: function (e) {
 			var keyCode = e.keyCode || e.which;
 			if (keyCode === 13){  // pressed enter key
@@ -188,23 +188,28 @@
 				return false;
 			}
 		},
-		
+
 		handleAutoCompletePlaceChange: function (autocomplete) {
 			var place = autocomplete.getPlace();
+			if (!place.geometry) {
+				// User entered the name of a Place that was not suggested and
+				// pressed the Enter key, or the Place Details request failed.
+				return;
+			}
 			var lat = place.geometry.location.lat();
 			var lng = place.geometry.location.lng();
 			this.updateLocationInput(lat, lng, place);
 			this.fitBoundMarker()
 		},
-		
-		
+
+
 		showOverlay: function(){
 			this.loaderOverlayElem.removeClass("hide")
 		},
-		
+
 		hideOverlay: function(){
 			this.loaderOverlayElem.addClass("hide")
 		}
 	});
-	
+
 })(mapWidgets.jQuery);
