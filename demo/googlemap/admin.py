bf6e7c54ac648eef9d3e14683337aceee7279371
erdem <github@erdemozkol.com>
2024-04-27 16:07:26 +0100

    remove static widgets

--- demo/googlemap/admin.py
+++ demo/googlemap/admin.py
@@ -24,30 +24,4 @@ class InteractivePointFieldAdmin(admin.ModelAdmin):
     inlines = [InteractiveAdminInline]
 
 
-class StaticPointFieldAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    formfield_overrides = {
-        models.PointField: {"widget": mapwidgets.GoogleMapPointFieldWidget}
-    }
-
-    def add_view(self, request, form_url="", extra_context=None):
-        return super().add_view(request, form_url, extra_context)
-
-    def get_form(self, request, obj=None, change=False, **kwargs):
-        if change:  # Catch if admin renders edit item page
-
-            class StaticPointFieldAdminForm(forms.ModelForm):
-                class Meta:
-                    fields = "__all__"
-                    model = googlemap_models.StaticPointField
-                    widgets = {
-                        "location": mapwidgets.GoogleMapPointFieldStaticWidget,
-                        "location_optional": mapwidgets.GoogleMapPointFieldStaticWidget,
-                    }
-
-            self.form = StaticPointFieldAdminForm
-        return super().get_form(request, obj, change, **kwargs)
-
-
 admin.site.register(googlemap_models.InteractivePointField, InteractivePointFieldAdmin)
-admin.site.register(googlemap_models.StaticPointField, StaticPointFieldAdmin)
