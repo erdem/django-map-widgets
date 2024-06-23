5d719f8f84899fae3b86da453f1117f1df37e55e
erdem <github@erdemozkol.com>
2024-06-23 21:02:56 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
-VERSION = (0, 4, 2)
+VERSION = (0, 5, 0)
 __version__ = ".".join(map(str, VERSION))
 
 
 LONG_DESCRIPTION = """
-Configurable, pluggable and more user friendly map widgets for Django PostGIS fields. 
+Configurable, pluggable and more user friendly map widgets for GeoDjango fields. 
 Please check the `project home page <https://github.com/erdem/django-map-widgets/>`_ for latest updates.
 """
 
@@ -17,15 +17,15 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/x-rst",
     author="Erdem Ozkol",
-    author_email="erdemozkol@gmail.com",
+    author_email="mapwidgets@erdemozkol.com",
     url="https://github.com/erdem/django-map-widgets",
     license="MIT",
     platforms=["any"],
-    packages=find_packages(exclude=("example", "static", "env")),
+    packages=find_packages(exclude=("demo", "static", "env")),
     include_package_data=True,
     classifiers=[
         "Environment :: Web Environment",
-        "Intended Audience :: Developers",
+        "Intended Audience :: Developers/End Users",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Framework :: Django",
