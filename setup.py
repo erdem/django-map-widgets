b29bb8ed974b12293b4eb8a53d5fa0ac39189a72
erdem <github@erdemozkol.com>
2024-06-23 23:07:26 +0100

    generate minified static files

--- setup.py
+++ setup.py
@@ -1,34 +1,42 @@
 from setuptools import find_packages, setup
+import os
+
 
 VERSION = (0, 5, 0)
 __version__ = ".".join(map(str, VERSION))
 
 
-LONG_DESCRIPTION = """
-Configurable, pluggable and more user friendly map widgets for GeoDjango fields. 
-Please check the `project home page <https://github.com/erdem/django-map-widgets/>`_ for latest updates.
-"""
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(BASE_DIR, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
 
 setup(
     name="django-map-widgets",
     version=__version__,
-    description="Map widgets for Django PostGIS fields",
-    long_description=LONG_DESCRIPTION,
+    description="Configurable and user-friendly map widgets for GeoDjango fields",
+    long_description=long_description,
     long_description_content_type="text/x-rst",
     author="Erdem Ozkol",
     author_email="mapwidgets@erdemozkol.com",
     url="https://github.com/erdem/django-map-widgets",
     license="MIT",
-    platforms=["any"],
-    packages=find_packages(exclude=("demo", "static", "env")),
+    packages=find_packages(exclude=("demo", "tests", "docs")),
     include_package_data=True,
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
-        "Intended Audience :: Developers/End Users",
+        "Framework :: Django",
+        "Intended Audience :: Developers/End-users",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Framework :: Django",
         "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    keywords="django map-widgets geodjango geolocation mapbox google map leaflet interactive-maps",
+    project_urls={
+        "Documentation": "https://django-map-widgets.readthedocs.io/",
+        "Source": "https://github.com/erdem/django-map-widgets",
+        "Demo Project": "https://github.com/erdem/django-map-widgets/tree/main/demo",
+    },
 )
