cfce28b5df320001f70b37c6179c7be2282f36e0
erdem <github@erdemozkol.com>
2024-07-09 18:31:25 +0100

    Add collectstatic check step to githubCI

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -135,6 +135,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
